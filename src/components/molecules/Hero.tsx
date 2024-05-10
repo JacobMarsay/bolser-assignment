fdc1b0b1e8bd4941106a7beaae3c7b821d55d3df
MARSAY\jmmar <jmmasray@outlook.com>
2024-05-11 00:53:31 +0100

    hero image added - tweaks to topbar - more generic mixin for positioning

---
+++ src/components/molecules/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>This is a Heading</h1>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -1,10 +1,11 @@
+import Hero from "../molecules/Hero";
 import Homepage from "../organisms/HomePage";
 import TopBar from "../organisms/TopBar";
 const Home = () => {
   return (
     <Homepage>
       <TopBar />
-      <h1>Homepage</h1>
+      <Hero />
     </Homepage>
   );
 };
