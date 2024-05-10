b4075ff2a32f6841bfbff0ca0220fe1ece32dbc0
MARSAY\jmmar <jmmasray@outlook.com>
2024-05-11 00:09:44 +0100

    Created top bar layout

--- src/components/organisms/TopBar.tsx
+++ src/components/organisms/TopBar.tsx
@@ -3,8 +3,12 @@ const TopBar = () => {
   return (
     <div className="topbar__wrapper">
       <div className="topbar__content">
-        <Icon name="logo" />
-        <Icon name="facebook" />
+        <div className="icon__wrapper left">
+          <Icon name="logo" />
+        </div>
+        <div className="icon__wrapper right">
+          <Icon name="facebook" />
+        </div>
       </div>
     </div>
   );
